1f6a7bc3ead94b015d9dc706871fcc0ccc5f16d9
Nick <nicholas.zinno@vtsd.com>
2020-01-08 23:07:41 -0500

    Ch10

--- Face.java
+++ Face.java
@@ -11,6 +11,11 @@ public class Face{
   private int EyeStartY;
   private int EyeWidth;
   private int EyeHeight;
+  private NoseColor n;
+  private int NoseStartX;
+  private int NoseStartY;
+  private int NoseWidth;
+  private int NoseHeight;
 
   public Face(){
     FaceStartX = 10;
@@ -18,12 +23,33 @@ public class Face{
     FaceWidth = 200;
     FaceHeight = 200;
     z = (255,255,0);
+    b =
+    EyeStartX = 60;
+    EyeStartY = 120;
+    EyeWidth = 25;
+    EyeHeight = 25;
+    n =
+    NoseStartX = 110;
+    NoseStartY = 110;
+    NoseWidth = 15;
+    noseHeight = 15;
   }
 
   public Face(int FaceStartX, int FaceStartY, Color z){
-    this.FacestartX = FaceStartYtartX;
+    this.FacestartX = FaceStartX;
     this.FacestartY = FaceStartY;
+    this.FaceWidth = FaceWidth;
+    this.FaceHeight = FaceHeight;
     this.z = z;
+    this.EyeStartX = EyeStartX;
+    this.EyesStartY = EyeStartY;
+    this.EyeWidth = EyeWidth;
+    this.EyeHeight =  EyeHeight;
+    this.b = b;
+    this.NoseStartX = NoseStartX;
+    this.NoseStartY = NoseStartY;
+    this.NoseWidth = NoseWidth;
+    this.NoseHeight = NoseHeight;
 
 
   }
